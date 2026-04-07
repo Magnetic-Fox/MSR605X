71478f5a951dac040a669f41383e44e5ae971c17
Magnetic-Fox <magnetic-fox@hotmail.com>
2026-04-07 19:16:09 +0200

    Debugged version #1

--- msr605x.py
+++ msr605x.py
@@ -175,9 +175,16 @@ class MSR605X:
 		
 	# Write automation method (slicing + sending)
 	def writeData(self, data):
-		dataChunks = self.dataSplit(data, 64)
-		for dataChunk in dataChunks:
-			self.hidDevice.write(self.reportID + self.dataFill(dataChunk, 64))
+		dataChunk1 = data[0:62]
+		dataChunksX = self.dataSplit(data[62:], 63)
+		dataChunks = [dataChunk1] + dataChunksX
+		
+		for index in range(len(dataChunks)):
+			if index == 0:
+				self.hidDevice.write(self.reportID + b"\x00" + self.dataFill(dataChunks[index], 62))
+			else:
+				self.hidDevice.write(self.reportID + self.dataFill(dataChunks[index], 63))
+		
 		return
 		
 	# Hard write automation method (filling + sending)
